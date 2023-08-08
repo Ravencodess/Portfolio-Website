20a1646b693181ed8d5c7c8877df5a1fef2872f2
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2023-08-09 00:25:26 +0100

    added hero

--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/header.css";
+import "../styles/components/hero.css";
 import "../styles/utils.css";
+
+const hamburgerMenu = document.querySelector("#hamburger");
+const mobileMenu = document.querySelector("#mobile-menu");
+
+const toggleMenu = () => {
+	const isOpened = hamburgerMenu.getAttribute("aria-expanded") === "true";
+	isOpened ? closeMenu() : openMenu();
+};
+
+const openMenu = () => {
+	mobileMenu.classList.add("active");
+	hamburgerMenu.setAttribute("aria-expanded", true);
+};
+
+const closeMenu = () => {
+	mobileMenu.classList.remove("active");
+	hamburgerMenu.setAttribute("aria-expanded", false);
+};
+
+hamburgerMenu.addEventListener("click", toggleMenu);
