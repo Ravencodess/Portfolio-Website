b54e55a5334a02e1ac5f6bf28ba4cff31f0bf5f2
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2023-08-10 01:16:52 +0100

    added about

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
+import "../styles/components/bio.css";
 import "../styles/utils.css";
 
 const hamburgerMenu = document.querySelector("#hamburger");
