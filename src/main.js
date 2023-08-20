96556dfe20bbfa40749a94f6ebc9109201546321
Ravencodess <115118888+Ravencodess@users.noreply.github.com>
2023-08-20 23:27:52 +0100

    adeded elevator pitch video

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/bio.css";
 import "../styles/components/feature.css";
+import "../styles/components/elevator.css";
 import "../styles/components/contact.css";
 import "../styles/components/footer.css";
 import "../styles/utils.css";
